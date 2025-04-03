a14e3c20c5cdedbfd003acd2bd6f86e65a0264a0
Pavel <pasha_w790@bk.ru>
2025-04-04 01:27:28 +0700

    creating forms for booking and adding rooms. added a function (is_available) to models.py added views (add_room,create_booking,room_list,booking_success)

--- bookings/forms.py
+++ bookings/forms.py
@@ -1,11 +1,20 @@
 from django import forms
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
+from .models import Room,Booking
 
 # Форма для регистрации пользователя
 class CustomUserCreationForm(UserCreationForm):
     email = forms.EmailField(required=True)
-
     class Meta:
         model = User
         fields = ('username', 'email', 'password1', 'password2')
+
+class RoomForm(forms.ModelForm):
+    class Meta:
+        model = Room
+        fields = ('number','description','price','photo',)
+class BookingForm(forms.ModelForm):
+    class Meta:
+        model = Booking
+        fields = ('room','check_in','check_out')
--- bookings/urls.py
+++ bookings/urls.py
@@ -5,4 +5,8 @@ urlpatterns = [
     path('register/', views.register, name='register'),
     path('login/', views.login_view, name='login'),
     path('home/',views.home_view, name='home'),
+    path('create_booking/', views.create_booking, name='create_booking'),
+    path('booking_success/', views.booking_success, name='booking_success'),
+    path('room_list/', views.room_list, name='room_list'),
+    path('add_room/', views.add_room, name='add_room'),
 ]
