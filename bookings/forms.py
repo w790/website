5e003c6790740f7cfb06ed39d96a0ffa12150e12
Pavel <pasha_w790@bk.ru>
2025-04-05 01:39:55 +0700

    added a check for booking availability. added booking management views for the administrator + added logic for users to cancel and view bookings.

--- bookings/forms.py
+++ bookings/forms.py
@@ -18,3 +18,22 @@ class BookingForm(forms.ModelForm):
     class Meta:
         model = Booking
         fields = ('room','check_in','check_out')
+
+    def clean(self):
+        cleaned_data = super().clean()
+        room = cleaned_data.get('room')
+        check_in = cleaned_data.get('check_in')
+        check_out = cleaned_data.get('check_out')
+
+        if check_in >= check_out:
+            raise forms.ValidationError("Дата выезда должна быть позже даты заезда.")
+
+        if room:
+            # Получаем пользователя, если он есть
+            user = getattr(self.instance, 'user', None)
+            # Проверим, доступен ли номер
+            booking = Booking(user=user, room=room, check_in=check_in, check_out=check_out)
+            if not booking.is_available():
+                raise forms.ValidationError("Этот номер уже забронирован на выбранные даты.")
+
+        return cleaned_data
