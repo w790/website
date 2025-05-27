022558954efdf0c2790d8c57c8d887e9198adc15
CoyD1 <apranovich22010kostya@mail.ru>
2025-05-28 01:07:02 +0700

    Создан базовый шаблон base.html Реальзована регистрация и вход пользователей Реализовано бронирование отелей и личный кабинет для их просмотра.

--- bookings/views.py
+++ bookings/views.py
@@ -11,12 +11,9 @@ def register(request):
     if request.method == 'POST':
         form = CustomUserCreationForm(request.POST)
         if form.is_valid():
-            form.save()
-            username = form.cleaned_data.get('username')
-            password = form.cleaned_data.get('password1')
-            user = authenticate(username=username, password=password)
-            login(request, user)
-            return redirect('home')  # Перенаправление на главную страницу
+            user = form.save()  # Сохраняет пользователя и хеширует пароль
+            login(request, user)  # Автоматический вход
+            return redirect('home')
     else:
         form = CustomUserCreationForm()
     return render(request, 'bookings/register.html', {'form': form})
