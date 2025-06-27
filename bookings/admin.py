12381dba9c29c82ff2db79196ba5b03981725832
Pavel <pasha_w790@bk.ru>
2025-06-27 17:49:02 +0700

    update

--- bookings/admin.py
+++ bookings/admin.py
@@ -1,4 +1,7 @@
 from django.contrib import admin
-from .models import Room,Booking
+from .models import Room, Booking, CustomUser,Notification
+
 admin.site.register(Room)
 admin.site.register(Booking)
+admin.site.register(CustomUser)
+admin.site.register(Notification)
